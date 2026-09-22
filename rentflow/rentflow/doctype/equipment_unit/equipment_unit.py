10cf2c0381b4dda1a54932b826859544fd3e0f87
KAVYASABAREESAN <kavya.sabareesan11@gmail.com>
2026-09-22 14:01:37 +0000

    first commit

---
+++ rentflow/rentflow/doctype/equipment_unit/equipment_unit.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2026, Kavya and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class EquipmentUnit(Document):
+	pass
+
+from frappe.model.document import Document
+from frappe.model.naming import getseries
+
+class EquipmentUnit(Document):
+    def autoname(self):
+    # select a project name based on customer
+    	prefix = f"{self.category}"
+    	series = getseries(prefix, 4)
+    	self.name = f"{prefix[:3]}-{series}"
